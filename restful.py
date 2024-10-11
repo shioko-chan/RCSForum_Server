be9ad2783caaf81ea3b93fa9649ae86ee9d2a1c7
Ayaya <3329058005@qq.com>
2024-10-11 18:18:15 +0800

    save

--- restful.py
+++ restful.py
@@ -4,10 +4,10 @@ import threading
 import requests
 import logging
 import signal
-import asyncio
+import time
 
 
-def get_app_access_token():
+def renew_app_access_token():
     try:
         res = requests.post(
             url="https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal",
@@ -19,7 +19,7 @@ def get_app_access_token():
         )
         data = res.json()
         if res.status_code == 200 and data["code"] == 0:
-            time_to_live = max(float(data["expire"]) - 10.0, 10.0)
+            time_to_live = max(float(data["expire"]) - 300.0, 300.0)
             app.config["APP_ACCESS_TOKEN"] = data["app_access_token"]
             app.logger.info(
                 f"Successfully update app access token, next update is {time_to_live} seconds later."
@@ -56,31 +56,14 @@ if __name__ == "__main__":
     api.add_resource(CheckIn, "/checkin")
     api.add_resource(Login, "/login")
 
-    stop_event = asyncio.Event()  # 用来控制协程的停止
-
-    async def update_app_token():
-        ttl = get_app_access_token()
+    def renew_app_token():
+        ttl = renew_app_access_token()
         while ttl:
-            await asyncio.sleep(ttl)
-            ttl = get_app_access_token()
+            time.sleep(ttl)
+            ttl = renew_app_access_token()
 
-    async def run_flask():
-        app.run(host="0.0.0.0", port=5000, debug=True)
+    threading.Thread(
+        target=renew_app_token, name="renew app access token", daemon=True
+    ).start()
 
-    async def main():
-        # 使用 gather 同时运行 Flask 和 token 更新逻辑
-        await asyncio.gather(run_flask(), update_app_token(), stop_event.wait())
-
-    def shutdown():
-        # 当接收到信号时，触发 stop_event 的停止
-        app.logger.info("Shutting down...")
-        stop_event.set()
-
-    # 监听系统信号 SIGINT 和 SIGTERM，注册到 shutdown 函数
-    signal.signal(signal.SIGINT, lambda sig, frame: shutdown())
-    signal.signal(signal.SIGTERM, lambda sig, frame: shutdown())
-
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        app.logger.info("Program exited")
+    app.run(host="0.0.0.0", port=5000, debug=True)
